8fb43dfea06d7937ca7cfa9488adcf093535dfd1
KKKKKJ <kwu@ucsd.edu>
2025-12-01 10:23:02 -0600

    changegoogletag

--- docs/assets/js/data/swconf.js
+++ docs/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1764526017',resources: [
+    cacheName: 'chirpy-1764606135',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
